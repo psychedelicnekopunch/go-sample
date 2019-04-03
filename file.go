6012ad5405fe496d616d50d8a603a264f09c6b34
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-04-03 17:19:08 +0900

    add

---
+++ file.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+
+func main() {
+	file, err := os.Open("images/sample.png")
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+	defer file.Close()
+
+
+	copyTo, err := os.Create("images/sample_copy.png")
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+	defer copyTo.Close()
+
+	// _, err = io.Copy(copyTo, file)
+
+	// if err != nil {
+	// 	fmt.Printf(err.Error())
+	// 	return
+	// }
+
+    fmt.Printf("copied\n")
+}

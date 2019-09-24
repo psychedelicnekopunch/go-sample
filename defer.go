0d140393d2612c95e6036f8390c5e335d2e5036f
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2019-09-24 15:28:42 +0900

    add

--- defer.go
+++ defer.go
@@ -6,15 +6,28 @@ import (
 
 
 func main() {
-	fmt.Printf("main 1\n")
+	fmt.Printf("main: 1\n")
 	test()
-	fmt.Printf("main 2\n")
+	fmt.Printf("main: 2\n")
+	test2()
 }
 
 
 func test() {
-	fmt.Printf("test 1\n")
-	defer fmt.Printf("test 2\n")
+	fmt.Printf("test: 1\n")
 
-	fmt.Printf("test 3\n")
+	defer fmt.Printf("test: 2\n")
+
+	fmt.Printf("test: 3\n")
+}
+
+
+func test2() {
+	fmt.Printf("test2: 1\n")
+
+	defer fmt.Printf("test2: 2\n")
+	defer fmt.Printf("test2: 3\n")
+	defer fmt.Printf("test2: 4\n")
+
+	fmt.Printf("test2: 5\n")
 }

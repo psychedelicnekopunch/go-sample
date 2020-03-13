4364843f35eb283bbc4cc39a35074c1287c1e125
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2020-03-13 15:12:39 +0900

    add

---
+++ intToFloat.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+	"fmt"
+	"math"
+)
+
+
+func main() {
+	var i int = 123
+	var i2 int = 3456
+
+	f := float64(i) / float64(i2)
+	fmt.Print(f)
+	fmt.Print("\n")
+	fmt.Print(math.Floor(f * 100))
+	fmt.Print("\n")
+}

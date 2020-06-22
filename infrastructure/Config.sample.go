d294110f4030712f18e415c12c96bef7d4b1dfef
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2020-06-22 11:28:48 +0900

    update

--- infrastructure/Config.sample.go
+++ infrastructure/Config.sample.go
@@ -21,23 +21,38 @@ package infrastructure
 // 	Slack struct {
 // 		APIToken string
 // 	}
+// 	Mail struct {
+// 		Gmail struct {
+// 			Addr string
+// 			Host string
+// 			Address string
+// 			Name string
+// 			Password string
+// 		}
+// 	}
 // }
-
+//
 // func NewConfig() *Config {
-
+//
 // 	c := new(Config)
-
+//
 // 	c.Aws.S3.Region = "ap-northeast-1"
 // 	c.Aws.S3.Bucket = "YOUR Bucket Name"
 // 	c.Aws.S3.AccessKeyID = "YOUR AWS Access key ID"
 // 	c.Aws.S3.SecretAccessKey = "YOUR AWS Secret Access Key"
-
+//
 // 	c.DB.Production.Host = "localhost"
 // 	c.DB.Production.Username = "username"
 // 	c.DB.Production.Password = "password"
 // 	c.DB.Production.DBName = "dbname"
-
+//
 // 	c.Slack.APIToken = "SLACK_API_TOKEN"
-
+//
+// 	c.Mail.Gmail.Addr = "smtp.gmail.com:587"
+// 	c.Mail.Gmail.Host = "smtp.gmail.com"
+// 	c.Mail.Gmail.Address = "sample@gmail.com"
+// 	c.Mail.Gmail.Name = "your name"
+// 	c.Mail.Gmail.Password = "password"
+//
 // 	return c
 // }

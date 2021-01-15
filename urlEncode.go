e8145058088f3e31433fa0b08e8c4e926517b505
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2021-01-15 14:48:47 +0900

    update

--- urlEncode.go
+++ urlEncode.go
@@ -7,7 +7,7 @@ import (
 
 
 func main() {
-	str := "❤️ 絵文字_ やゔぁぴぼ _ 空白 \" ' ? ! .xlsx .png ♡"
+	str := "?q=test&❤️ 絵文字_ やゔぁぴぼ _ 空白 \" ' ? ! .xlsx .png ♡"
 	fmt.Print(str, "\n\n")
 	fmt.Print(url.PathEscape(str), "\n\n")
 	fmt.Print(url.QueryEscape(str), "\n\n")

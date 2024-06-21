fa8db06cf90e8c6f0d64d4719db399930a605fed
masa <psychedelic.nekopunch@gmail.com>
2024-06-21 15:02:31 +0900

    update

--- app/divide.go
+++ app/divide.go
@@ -8,5 +8,16 @@ import (
 
 func main() {
 	fmt.Print("0 / 10 = ", 0 / 10, "\n")
-	fmt.Print("10 / 0 = ", 10 / 0, "\n")
+	// fmt.Print("10 / 0 = ", 10 / 0, "\n")// invalid operation: division by zero
+	fmt.Print("10 / 0 = ", float64(10) / getZeroAsFloat64(), "\n")
+	fmt.Print("10 / 0 = ", 10 / getZeroAsInt(), "\n")// panic: runtime error: integer divide by zero
+}
+
+
+func getZeroAsFloat64() float64 {
+	return 0
+}
+
+func getZeroAsInt() int {
+	return 0
 }
